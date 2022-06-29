ad1c82cc8cc97e3541d07422dc544bd3783a29a3
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-06-29 04:33:22 +0000

    correct task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+count = 1
 for w in range(0, 9):
-    for i in range(1, 10):
+    for i in range(count, 10):
         if w == 8 and i == 9:
             print("{}".format(89))
             continue
         print("{}{}, ".format(w, i), end="")
+    count = count + 1
