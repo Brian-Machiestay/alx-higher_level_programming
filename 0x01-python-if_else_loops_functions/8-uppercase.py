d8e4948da1e36a2b28ac22dbacbe7d74a9b49078
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-06-30 04:47:04 +0000

    corrects task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     low = list(range(97, 123))
     upper = list(range(65, 91))
     for i in str:
-        if ord(i) in low:
-            print("{}".format(chr(upper[low.index(ord(i))])), end="")
-            continue
-        print("{}".format(i), end="")
+        j = ord(i)
+        print("{}".format(chr(upper[low.index(j)])) if j in low else i, end="")
+    print()
