d4dad592b88e573368a4cc2890992ca9f49a6efb
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-03 05:41:17 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if lenth == 1:
         print("{} arguments.".format(0))
     elif lenth == 2:
-        print("{}: argument".format(1))
+        print("{} argument:".format(1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(lenth - 1))
