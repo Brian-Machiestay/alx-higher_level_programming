7ba8415455f50ac0a4d16a72748931fb26542936
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-03 05:36:51 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    lenth = len(sys.argv)
+    if lenth == 1:
+        print("{} arguments.".format(0))
+    elif lenth == 2:
+        print("{}: argument".format(1))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(lenth - 1))
+        for i in range(1, lenth):
+            print("{}: {}".format(i, sys.argv[i]))
