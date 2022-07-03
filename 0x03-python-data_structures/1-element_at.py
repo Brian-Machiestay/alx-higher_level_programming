bced8d02de1a73ecd02443ef01a7a9923c15959f
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-03 19:29:42 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lenth = len(my_list)
-    if idx < 0 or idx > lenth:
+    if idx < 0 or idx > lenth - 1:
         return None
     return my_list[idx]
