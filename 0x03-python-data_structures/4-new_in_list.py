ffb8fde0f2d1a3f42801d693aefce6ba8e582a65
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-04 06:19:27 +0000

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    lent = len(my_list)
+    new_list = my_list[:]
+    if idx < 0 or idx > lent:
+        return new_list
+    new_list[idx] = element
+    return new_list
