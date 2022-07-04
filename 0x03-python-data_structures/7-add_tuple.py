f568df3a8515c0efbc28987bccac155b2d17f5e7
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-04 19:05:14 +0000

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        a1 = 0
+        a2 = 0
+    if len(tuple_b) == 0:
+        b1 = 0
+        b2 = 0
+    if len(tuple_a) == 1:
+        a2 = 0
+        a1 = tuple_a[0]
+    if len(tuple_b) == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    if len(tuple_a) >= 2:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+    if len(tuple_b) >= 2:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+    return a1 + b1, a2 + b2
