485f091e721b54f1ece9e399c1fc7a24c7f81f2c
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-04 19:44:17 +0000

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    a = my_list[0]
+    for element in my_list:
+        if element > a:
+            a = element
+    return a
