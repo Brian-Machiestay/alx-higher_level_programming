2f0813c77f62ddd1374b9cd81ef25eec468da423
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-07 07:09:01 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,9 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,
-             "D": 500, "M": 1000, "IX": 9, "IV": 4}
-    if roman_string == "IX" or roman_string == "IV":
+             "D": 500, "M": 1000, "IX": 9, "IV": 4, "XL": 40,
+             "XC": 90, "CD": 400, "CM": 900}
+    if roman_string in list(roman):
         return roman[roman_string]
     count = 0
     for i in range(0, len(roman_string)):
