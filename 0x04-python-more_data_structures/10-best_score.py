73ab82a013812aeecdc7b31a7b892fd8a69ade47
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-07 06:00:24 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == dict():
         return None
-    if a_dictionary == dict():
-        return
     count = a_dictionary[list(a_dictionary)[0]]
     for key in list(a_dictionary):
         if a_dictionary[key] > count:
