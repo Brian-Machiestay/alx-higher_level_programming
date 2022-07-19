9a2ec6817bae37f44cef228ad31c5bae82547f3b
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-19 02:24:32 +0000

    safe prints integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for e in range(0, x):
+        try:
+            print("{:d}".format(my_list[e]), end="")
+            j = j + 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return j
