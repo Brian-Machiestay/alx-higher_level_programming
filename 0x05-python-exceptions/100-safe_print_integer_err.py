fb1de0e3f1b197701b2749f5d0c7bea5263c2628
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-19 04:38:16 +0000

    prints error to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as ne:
+    except (TypeError, ValueError) as ne:
         print("Exception: {}", ne, file=sys.stderr)
         return False
