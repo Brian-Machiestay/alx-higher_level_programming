5d2e45f49db4c50344d93cec427a28a54cffbc5f
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-19 21:59:31 +0000

    adds documentation to the file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """creates an empty class that defines a square"""
-    pass
+    def __init__(self):
+        """init initializes an instance of this class"""
+        pass
