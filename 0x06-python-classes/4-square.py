580c55214f4d3712ef033c44cb36fbb65750ab39
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-20 17:07:07 +0000

    creates getter and setter methods for size

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creates a square module and instantiates it"""
+
+
+class Square:
+    """the square class"""
+    def __init__(self, size=0):
+        """instantiates the size attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size"""
+        self.__init__(value)
