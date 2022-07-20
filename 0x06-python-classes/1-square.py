48dce135530e97d3d478a1a3d703df2947af4950
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-20 04:57:30 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """Square defines a square"""
-    def __init__(self, size):
+    def __init__(self, size=""):
         """initializes the the square.
 
         Args:
