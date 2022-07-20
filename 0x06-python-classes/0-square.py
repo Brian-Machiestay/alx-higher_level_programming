9208990333b0a7c07498128e1444557b0eb98aa7
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-20 05:26:19 +0000

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 class Square:
     """creates an empty class that defines a square"""
-    def __init__(self):
-        """init initializes an instance of this class"""
-        pass
+    pass #an empty block
