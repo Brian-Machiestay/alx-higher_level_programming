da326a45e5a98856be6967fba302a97f56d52d23
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-20 16:24:23 +0000

    checked instantiation value

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""creates a square module and instantiates it"""
+
+
+class Square:
+    """the square class"""
+    def __init__(self, size=0):
+        """instantiates the size attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
