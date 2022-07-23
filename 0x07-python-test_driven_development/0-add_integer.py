f1f578429e4b8e6fba8082ac718fef994da75d79
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-23 16:04:15 +0000

    does task one and its test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/bin/usr/python3
+"""this module adds two integers
+defines the function add_integer
+which adds two integers
+
+"""
+
+
+def add_integer(a, b=98):
+    """this function adds two integers
+    takes the integer values a and b and adds them
+
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    thisa = int(a)
+    thisb = int(b)
+    return thisa + thisb
