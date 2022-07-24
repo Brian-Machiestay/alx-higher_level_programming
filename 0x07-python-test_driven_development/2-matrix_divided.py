d0a21671fd3b0af2a9e91d839bae5369a27ac3c1
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-07-24 12:18:31 +0000

    creates a matrix division module and tests it

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/bin/usr/python3
+"""this module divides a
+a matrix and returns a new
+matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    new_Mat = []
+    new_Mat_cont = []
+    size = 0
+    changingsize = 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+    if matrix == []:
+        raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+        else:
+            if size == 0:
+                size = len(matrix[0])
+            if len(i) != size:
+                raise TypeError("Each row of the matrix must\
+ have the same size")
+            for j in i:
+                if type(j) is not float and type(j) is not int:
+                    raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+                divide = round(j / div, 2)
+                new_Mat_cont.append(divide)
+            new_Mat.append(new_Mat_cont)
+            new_Mat_cont = []
+    return new_Mat
