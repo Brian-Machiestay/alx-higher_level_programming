31dd5e285e2409b30155fd7103c28c4971b3a7ee
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-08-02 04:22:56 +0000

    task 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""creates an empty geometry"""
+
+
+class BaseGeometry:
+    """empty geometry"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""creates no more empty geometry"""
+
+
+class BaseGeometry:
+    """no more empty geometry"""
+
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
