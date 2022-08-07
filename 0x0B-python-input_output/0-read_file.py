d59f169a61b96e7cb37d74515b36587819b7baf1
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-08-07 06:45:58 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module reads from a file"""
+
+
+def read_file(filename=""):
+    """this functions reads a file"""
+
+    with open(filename, "r", encoding="UTF-8") as thisfile:
+        print(thisfile.read())
