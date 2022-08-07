408125416c0b4316b84d998920b5f1001cd20db3
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-08-07 13:41:39 +0000

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this module creates a student class"""
+
+
+class Student:
+    """this is the student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary repr of this instance"""
+        thisdict = dict()
+        if type(attrs) is list:
+            for i in list(self.__dict__):
+                for j in attrs:
+                    if i == j:
+                        thisdict[j] = self.__dict__[i]
+            return thisdict
+        else:
+            return self.__dict__
