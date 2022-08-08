0c341929dd206da5647877c13ab95fb54197b3ff
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-08-08 06:23:14 +0000

    task advanced

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""checks if there is a
+a particular string and inserts a text on next line
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """does the job"""
+    lines = ""
+    with open(filename, "r", encoding="UTF-8") as f:
+        lines = f.readlines()
+    with open(filename, "r", encoding="UTF-8") as f:
+        count = 0
+        for i in f:
+            size = len(search_string)
+            while size < len(i):
+                if i[size - len(search_string): size] == search_string:
+                    lines.insert(count + 1, new_string)
+                    print("we found him on line {}".format(count))
+                    count = count + 1
+                size = size + 1
+            size = 4
+            count = count + 1
+    lines = "".join(lines)
+    with open(filename, "w", encoding="UTF-8") as f:
+        f.write(lines)
