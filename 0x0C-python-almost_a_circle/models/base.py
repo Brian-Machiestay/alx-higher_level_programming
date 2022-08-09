8d93bb2efb58ece5ee9925f9a8e85f9c1978cfe5
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-08-09 05:54:53 +0000

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """this is the base class"""
 
 
+import json
+
+
 class Base:
     """the base class"""
 
@@ -14,3 +17,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return json repre"""
+        if list_dictionaries is None or list_dictionaries == "":
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save json repr of listobjs to a file"""
+        with open("{}.json".format(cls), "w", encoding="UTF-8") as f:
+            f.write(Base.to_json_string(list_objs))
