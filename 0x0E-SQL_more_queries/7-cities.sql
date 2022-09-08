d785941cf2da97b336ed0421fd62eb3124be9058
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-08 05:58:02 +0000

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,1 +1,5 @@
 -- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT, state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL)
+type=InnoDB;
