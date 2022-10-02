daed39e511dc743df5d21f46d379f71bf457bf63
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-02 19:10:59 +0000

    all shows with genres

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,7 @@
 -- shows contained in the database hbtn_0d_tvshows
-SELECT title, genre_id FROM tv_show_genres,
-tv_genres,tv_shows
-WHERE tv_shows.id=tv_show_genres.show_id AND
-tv_show_genres.genre_id=tv_genres.id
-ORDER BY tv_shows.title,tv_show_genres.genre_id;
+SELECT sh.title,genre_id
+FROM tv_shows AS sh
+LEFT JOIN tv_genres ON sh.id=tv_genres.id
+LEFT JOIN tv_show_genres ON
+tv_genres.id=tv_show_genres.show_id
+ORDER BY sh.title,tv_show_genres.genre_id;
