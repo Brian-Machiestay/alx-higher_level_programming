6c378ce4707241ee2710c290b2f8964f928e8a42
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-02 14:10:20 +0000

    joins two tables without join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT DISTINCT states.id, cities.name, states.name
+SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.id = state_id
-ORDER BY states.id
+WHERE states.id=state_id
+ORDER BY cities.id
