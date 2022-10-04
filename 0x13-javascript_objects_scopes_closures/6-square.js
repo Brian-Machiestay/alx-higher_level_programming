083b79508d7de81f9208a14f3ca9e46c9eaf2271
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-04 22:10:43 +0000

    draw with c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,10 @@ const superSquare = require('./5-square.js');
 module.exports = class Square extends superSquare {
   charPrint (c) {
     if (!c) this.print();
-    else if (c !== 'C') this.print();
     else {
       let square = '';
       for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) square = square + 'C';
+        for (let j = 0; j < this.width; j++) square = square + c;
         if (i < this.height - 1) square = square + '\n';
       }
       console.log(square);
