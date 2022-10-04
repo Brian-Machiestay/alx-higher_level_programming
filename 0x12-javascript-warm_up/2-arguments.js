23b8722c16228f334d99715db2f8579e4b089c73
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-04 06:25:36 +0000

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,4 +2,6 @@
 require('node:process');
 const argv = process.argv;
 const len = argv.length;
-if (len === 2) { console.log('No argument'); } else if (len === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (len === 2) console.log('No argument');
+else if (len === 3) console.log('Argument found');
+else console.log('Arguments found');
