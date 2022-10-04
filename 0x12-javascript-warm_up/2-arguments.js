9b54df287f5405e06fe2391b7fa0f4a010a50eac
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-04 13:03:26 +0000

    process argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-require('node:process');
-const argv = process.argv;
+import { argv } from 'node:process';
+
 const len = argv.length;
 if (len <= 2) console.log('No argument');
 else if (len === 3) console.log('Argument found');
