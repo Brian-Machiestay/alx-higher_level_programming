b3133a8dd61d52c0febb6fb08888bb9d64673bcd
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-04 22:02:14 +0000

    print with c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const superSquare = require('./5-square.js');
+module.exports = class Square extends superSquare {
+  charPrint (c) {
+    if (!c) this.print();
+    else {
+      let square = '';
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) square = square + 'C';
+        if (i < this.height - 1) square = square + '\n';
+      }
+      console.log(square);
+    }
+  }
+};
