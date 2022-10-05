0bd4ba601a50f2bcc84647bcc7f190c54f7fbb01
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-05 13:19:40 +0000

    concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+require('process');
+const fs = require('fs');
+const argv = process.argv;
+fs.readFile(argv[2].toString(), 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(argv[4].toString(), data + '\n', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+fs.readFile(argv[3].toString(), 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  fs.appendFile(argv[4].toString(), data + '\n', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
