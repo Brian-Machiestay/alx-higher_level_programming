763cc149a8e9d2f6dace171626232d2f6d4e2cdc
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-08 12:42:42 +0000

    filter the state mysqldb fun

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE  UPPER(name) LIKE 'n%' ORDER BY states.id")
+
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
