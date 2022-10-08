48a889732103d33b83ed02628de99ad28c3b7fb2
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-08 11:56:08 +0000

    connect and query mysql using python dbAPI

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" this module connects to mysql database using the API"""
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+
+    username = argv[1]
+    passwrd = argv[2]
+    database = argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=passwrd,
+        db=database,
+        charset="utf8"
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row.name))
