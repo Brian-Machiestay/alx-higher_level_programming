2e9269ac730d6c72e3b7c4fbf8065fead831601d
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-20 22:55:01 +0000

    body if 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # returns the body of a status 200 response
-if [ "$(curl -s -I "$1" | grep "HTTP" | grep -o -e "[0-9][0-9][0-9]")" -eq 200 ]; then curl -s -G "$1"; fi
+if [ "$(curl -s -I "$1" | grep "HTTP" | grep -o -e "[0-9][0-9][0-9]")" -eq 200 ]; then curl -s "$1"; fi
