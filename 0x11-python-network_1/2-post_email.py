de4f72e7f74648e9a5aa4f9233eb115cf4bfe88a
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-22 05:57:58 +0000

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     res = req.Request(sys.argv[1], method="POST")
     res.data = {"email": sys.argv[2]}
     with req.urlopen(res) as ff:
-        print(ff.read())
+        print(ff.read().decode('utf-8'))
