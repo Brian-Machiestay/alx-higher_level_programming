0427df4b6cafca520c7db2d8193398cff05f787a
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-22 05:09:56 +0000

    urllib request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""use the urllib module to fetch a webpage"""
+import urllib.request as req
+import json
+
+
+fetched = req.Request('https://alx-intranet.hbtn.io/status')
+with req.urlopen(fetched) as ff:
+    content = ff.read()
+    print("Body response:")
+    print("\t-type: {}".format(type(content)))
+    print("\t-content: {}".format(content))
+    print("\t-utf8 content: {}".format(content.decode()))
