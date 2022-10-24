0ac5cac942c5fe589e8b8cbf6b6d0400b46f75a0
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-24 05:21:36 +0000

    print the http error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""return the value of a specific header"""
+import sys
+import urllib.request as req
+import urllib.error
+
+if __name__ == "__main__":
+    res = req.Request(sys.argv[1])
+    try:
+        with req.urlopen(res) as ff:
+            body = ff.read().decode('utf-8')
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
