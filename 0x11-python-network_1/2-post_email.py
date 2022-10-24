9f1771c8c45053cabae82d982e40329a3adc3831
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-24 05:03:15 +0000

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ import urllib.request
 import urllib.parse
 
 if __name__ == "__main__":
-    data = {"email" : sys.argv[2]}
+    data = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
     res = urllib.request.Request(sys.argv[1], data)
