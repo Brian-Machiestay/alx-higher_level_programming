25d11e6f8fa3c58703535092c8a6e816be7f817d
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-15 13:02:37 +0000

    print the status code of a get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ const url = argv[2];
 
 request(url, function (error, response, body) {
   if (error) console.error(error);
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
