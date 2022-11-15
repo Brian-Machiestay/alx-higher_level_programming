11796b61fc4331274bf60c60a5d03a8062b3aec5
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-15 15:17:40 +0000

    count the number of movies 18 featured in

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,8 +7,10 @@ const character = 'https://swapi-api.hbtn.io/api/people/' + 18 + '/';
 request(starApi, function (error, response, body) {
   if (error) console.error(error);
   let count = 0;
-  for (const obj of JSON.parse(body).results) {
-    if (obj.characters.includes(character)) count++;
+  if (JSON.parse(body).results) {
+    for (const obj of JSON.parse(body).results) {
+      if (obj.characters.includes(character)) count++;
+    }
   }
   console.log(count);
 });
