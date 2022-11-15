8a5810349296206b19c7994aadfab7e613f09d23
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-15 13:00:40 +0000

    print the status code of a get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+const argv = process.argv;
+
+const url = argv[2];
+
+request(url, function (error, response, body) {
+  if (error) console.error(error);
+  console.log('code: ', response.statusCode);
+});
