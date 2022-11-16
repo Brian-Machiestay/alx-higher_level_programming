cc8cd0eae604e9231d9470491c117d7a506b8844
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-16 05:48:24 +0000

    in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,16 +3,19 @@ const request = require('request');
 const argv = process.argv;
 const id = argv[2];
 const film = 'https://swapi-api.hbtn.io/api/films/' + id + '/';
-async req(film) {
-  request(film, function (error, response, body) {
+async function reqq (film) {
+  await request(film, function (error, response, body) {
     if (error) console.error(error);
-    const parsedBody = JSON.parse(body);
-    for (const obj of parsedBody.characters) {
-      await request(obj, function (error, response, bo) {
-        if (error) console.error(error);
-        console.log(JSON.parse(bo).name);
-      });
+    async function req () {
+      const parsedBody = JSON.parse(body);
+      for (const obj of parsedBody.characters) {
+        await request(obj, function (error, response, bo) {
+          if (error) console.error(error);
+          console.log(JSON.parse(bo).name);
+        });
+      }
     }
-  })
-};
-req(film);
+    req();
+  });
+}
+reqq(film);
